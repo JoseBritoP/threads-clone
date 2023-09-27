115690102d1b80c5fe38a371068cc514aa4969f7
JoseBritoP <josealbritose@gmail.com>
2023-09-27 04:52:34 -0300

    Create thread page

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,8 +1,21 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from 'next/navigation'
+import { getUser } from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/PostThread";
+
 async function Page(){
 
+  const user = await currentUser();
+
+  if(!user) return null;
+
+  const userInfo = await getUser(user.id);
+
+  if(!userInfo?.onboarded) redirect('/onboarding');
   return (
     <>
-      <h1 className="text-white text-xl font-semibold">Create Threads</h1>
+      <h1 className="text-white text-2xl font-semibold">Create Threads</h1>
+      <PostThread userId={userInfo._id}/>
     </>
   )
 }
