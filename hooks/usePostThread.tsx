7087ec5ba21877216e6d22cc0e08d21c85093ba9
JoseBritoP <josealbritose@gmail.com>
2023-09-27 04:54:19 -0300

    custom hook usePostThread Form

---
+++ hooks/usePostThread.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useOrganization } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.actions";
+
+const usePostThread = (userId:string) => {
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { organization } = useOrganization();
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: organization ? organization.id : null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
+
+  return { form, onSubmit }
+
+};
+
+export default usePostThread;
