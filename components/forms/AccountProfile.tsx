38f1f033622f357b3dc74129ae2fdaad16b719e1
JoseBritoP <josealbritose@gmail.com>
2023-09-27 04:52:58 -0300

    AccountProfile agree FormMessage

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -58,6 +58,7 @@ const AccountProfile = ({user,btnTitle}:Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
