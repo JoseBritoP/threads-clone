753ccd05d5a9fc29e70ab06b67230235a3a63a14
JoseBritoP <josealbritose@gmail.com>
2023-09-26 23:16:15 -0300

    Api folder

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,28 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page () {
+
+  const user = await currentUser()
+  
+  const userInfo = {
+
+  }
+  const userData = {
+    id:user?.id || "",
+    objectId: userInfo?._id || "",
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl
+  }
   return (
-    <main>
-      <h1 className="head-text text-black">Onboarding</h1>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-10">
+      <h1 className="text-2xl text-white">Onboarding</h1>
+      <p className="mt-3 text-base text-slate-100">Complete your profile to use Threads</p>
+      <section className="mt-9 bg-[#111] p-10 rounded-md">
+        <AccountProfile user={userData} btnTitle="Continue"/>
+      </section>
     </main>
   )
 }
